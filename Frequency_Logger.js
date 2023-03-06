cfca6510989cdae4d52c03dc8fcce2166fd1fb0e
Mobilegamer64 <marcel.kitzbichler@gmail.com>
2023-03-06 19:17:59 +0100

    Added some error Handling

--- Frequency_Logger.js
+++ Frequency_Logger.js
@@ -3,27 +3,33 @@ const fs = require("fs");
 let log = [];
 let i = 0;
 
-let yourToken = "yourToken";
+let yourToken = "YourTokenHere";
 
 async function getFrequency() {
-    let response = await fetch(`https://api.gridradar.net/query?token=${yourToken}&metric=frequency-ucte-median-1s`);
-    let data = await response.json();
+    try{
+        let response = await fetch(`https://api.gridradar.net/query?token=${yourToken}&metric=frequency-ucte-median-1s`)
+        let data = await response.json();
     
-    if (Array.isArray(data)) {
-        const Datapoints = data[0].datapoints;
-        currentFrequency = Datapoints[Datapoints.length - 1];
-        console.log(currentFrequency);
-        log[i] = { frequency: currentFrequency[0], timestamp: currentFrequency[1] };
-        i++;
-        fs.writeFileSync("Log.json", JSON.stringify(log));
+        if (Array.isArray(data)) {
+            const Datapoints = data[0].datapoints;
+            currentFrequency = Datapoints[Datapoints.length - 1];
+            console.log(currentFrequency);
+            log[i] = { frequency: currentFrequency[0], timestamp: currentFrequency[1] };
+            i++;
+            fs.writeFileSync("Log.json", JSON.stringify(log));
         
-        setTimeout(getFrequency, 15000);
+            setTimeout(getFrequency, 15000);
         
-        return;
-    }
+            return;
+        }
 
-    console.log("API Error");
+        console.log("API Error");
     
-    setTimeout(getFrequency, 15000);
+        setTimeout(getFrequency, 15000);
+    }
+    catch {
+        console.log("Unable to reach API"); setTimeout(getFrequency, 15000); return;
+    }
+    
 }
 getFrequency();
