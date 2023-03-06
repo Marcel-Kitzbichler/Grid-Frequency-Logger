ab1d774bd7a9dd34626cabde8fcd24a3496909fb
Mobilegamer64 <marcel.kitzbichler@gmail.com>
2023-03-06 11:47:27 +0100

    Made the Code more Beautiful

--- Frequency_Logger.js
+++ Frequency_Logger.js
@@ -1,23 +1,29 @@
+const fs = require("fs");
+
 let log = [];
 let i = 0;
-const fs = require("fs");
 
 let yourToken = "yourToken";
 
-async function getFrequency(){
-    let response = await fetch("https://api.gridradar.net/query?token="+yourToken+"&metric=frequency-ucte-median-1s");
+async function getFrequency() {
+    let response = await fetch(`https://api.gridradar.net/query?token=${yourToken}&metric=frequency-ucte-median-1s`);
     let data = await response.json();
-    if(Array.isArray(data) == true){
+    
+    if (Array.isArray(data)) {
         const Datapoints = data[0].datapoints;
         currentFrequency = Datapoints[Datapoints.length - 1];
         console.log(currentFrequency);
-        log[i] = {frequency: currentFrequency[0], timestamp: currentFrequency[1]};
+        log[i] = { frequency: currentFrequency[0], timestamp: currentFrequency[1] };
         i++;
         fs.writeFileSync("Log.json", JSON.stringify(log));
+        
+        setTimeout(getFrequency, 15000);
+        
+        return;
     }
-    else{
-        console.log("API Error");
-    }
+
+    console.log("API Error");
+    
     setTimeout(getFrequency, 15000);
 }
 getFrequency();
